d884c013955ed0239009aec85773044cf0ce9d0f
ZixiaoWang <www.wangzixiao@gmail.com>
2019-10-29 21:25:30 +0800

    Added station_location.json

--- functions/query.js
+++ functions/query.js
@@ -2,7 +2,8 @@ const fs = require('fs-extra');
 const axios = require('axios');
 const _ = require('lodash');
 
-const endpoint = 'https://kyfw.12306.cn/otn/queryTrainInfo/query?leftTicketDTO.train_no=2400000D2713&leftTicketDTO.train_date=2019-10-28&rand_code=';
+const api_0 = 'https://kyfw.12306.cn/otn/leftTicketPrice/query?leftTicketDTO.train_date=2019-10-29&leftTicketDTO.from_station=NJH&leftTicketDTO.to_station=SHH&leftTicketDTO.ticket_type=1&randCode=';
+const api_1 = 'https://kyfw.12306.cn/otn/queryTrainInfo/query?leftTicketDTO.train_no=2400000D2713&leftTicketDTO.train_date=2019-10-28&rand_code=';
 const api = 'https://kyfw.12306.cn/otn/queryTrainInfo/query';
 
 const query = async ({ date, no }) => {
