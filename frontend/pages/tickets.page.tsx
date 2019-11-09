75332c86bc3225432c4439b3071d0e95d17c7605
ZixiaoWang <www.wangzixiao@gmail.com>
2019-11-09 22:27:17 +0800

    Updated tickets page

--- frontend/pages/tickets.page.tsx
+++ frontend/pages/tickets.page.tsx
@@ -1,8 +1,9 @@
 import { h, Component, Fragment } from 'preact';
 import { route } from 'preact-router';
 
-export class TicketsPage extends Component {
+const left_ticket_api: string = 'https://kyfw.12306.cn/otn/queryTrainInfo/query';
 
+export class TicketsPage extends Component {
     render () {
         return null;
     }
