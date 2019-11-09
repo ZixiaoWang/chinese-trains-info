4ee565d4a8295ee4d769873f8756169582fefe3e
ZixiaoWang <www.wangzixiao@gmail.com>
2019-11-09 22:24:55 +0800

    Created tickets page

---
+++ frontend/pages/tickets.page.tsx
@@ -0,0 +1,9 @@
+import { h, Component, Fragment } from 'preact';
+import { route } from 'preact-router';
+
+export class TicketsPage extends Component {
+
+    render () {
+        return null;
+    }
+}
