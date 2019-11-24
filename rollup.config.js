94585f555918c7d950dfb10f75f518c41cad7909
ZixiaoWang <www.wangzixiao@gmail.com>
2019-11-24 16:19:16 +0800

    Changed project structure

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,27 @@
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
+import copy from 'rollup-plugin-copy';
 import scss from 'rollup-plugin-scss';
 
 export default {
-    input: 'frontend/index.tsx',
+    input: './frontend/index.tsx',
     output: {
-        file: './main.js',
+        file: './static/main.js',
         format: 'cjs',
         intro: 'const process = { env: { NODE_ENV: null } };'
     },
     plugins: [
-        typescript(),
+        typescript({
+            clean: true,
+            objectHashIgnoreUnknownHack: false
+        }),
         commonjs(),
         resolve(),
-        scss({ output: 'main.css' })
+        scss({ output: './static/main.css' }),
+        copy({
+            targets: [{ src: './frontend/index.html', dest: './static' }],
+            verbose: true
+        })
     ]
 }
